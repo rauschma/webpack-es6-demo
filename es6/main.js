f42f4d72af4069a4541a17647e2eecb2d8e439af
rauschma <axel@rauschma.de>
2015-04-02 01:46:58 +0200

    First commit

---
+++ es6/main.js
@@ -0,0 +1,3 @@
+import Point from './Point';
+var body = document.querySelector('body');
+body.textContent = 'Good point: ' + new Point(1, 23);
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+module.exports = {
+    entry: './es6/main.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: path.join(__dirname, 'es6'), loader: 'es6-loader' }
+        ]
+    }
+};
