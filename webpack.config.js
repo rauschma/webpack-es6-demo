dd160390e3a895506d4934719d18d8da9ddfc532
rauschma <axel@rauschma.de>
2015-04-02 03:12:48 +0200

    Switched to babel-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: path.join(__dirname, 'es6'), loader: 'es6-loader' }
+            { test: path.join(__dirname, 'es6'), loader: 'babel-loader' }
         ]
     }
 };
