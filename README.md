9fc714f589669075e38a5b5f3f6927a3aa219bfc
rauschma <axel@rauschma.de>
2015-04-02 02:03:18 +0200

    Readme file plus minor improvements.

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack ES6 demo
+
+A small demo project that shows how to use webpack for client-side development in ECMAScript 6.
--- es6/main.js
+++ es6/main.js
@@ -1,3 +1,3 @@
-import Point from './Point';
+import Point from './Point.js';
 var body = document.querySelector('body');
 body.textContent = 'Good point: ' + new Point(1, 23);
